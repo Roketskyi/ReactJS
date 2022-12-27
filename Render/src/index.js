fdeb7b1a6e363c157767eda62b87aa33f0b956ea
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2022-12-28 01:44:05 +0300

    Update index.js

--- Render/src/index.js
+++ Render/src/index.js
@@ -4,19 +4,14 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
 
 function clock() {
-  const element = (
+  const el = (
     <div>
       <h2>This is Clock!</h2>
       <h3>Time is {new Date().toLocaleTimeString()}</h3>
     </div>
   );
-  root.render(element);
+  root.render(el);
 }
 setInterval(clock, 0);
