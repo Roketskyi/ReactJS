c020b4ba474b946cfa19c05ed49c7b57707e5bd1
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-01-12 01:53:02 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import { Homepage } from './pages/homepage/Homepage';
+import { Posts } from './pages/posts/Posts';
+import { PostsTitle } from './pages/posts/PostsTitle';
+import { Photos } from './pages/photos/Photos';
+import { About } from './pages/about/About';
+import { Notfound } from './pages/notfound/Notfound';
+import { Loginpage } from './auth/Loginpage';
+
+import { Breadbord } from './сomponents/breadbord/Breadbord';
+
+function App() {
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleLogin = () => setIsLogin(true);
+  const handleLogout = () => setIsLogin(false);
+
+  return (
+    <>
+      <Routes> 
+        <Route path="/" element={<Breadbord isLogin={isLogin} handleLogin={handleLogin} handleLogout={handleLogout} />} >
+          <Route index element={<Homepage />} />
+          <Route path="posts" element={<Posts />} />
+          <Route path="posts/:title" element={<PostsTitle />} />
+          <Route path="photos" element={<Photos />} />
+          <Route path="about" element={<About />} />
+          <Route path="login" element={<Loginpage setIsLogin={setIsLogin} />} />
+        
+          <Route path="*" element={<Notfound />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App;
