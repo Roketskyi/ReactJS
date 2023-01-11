f1e4f2345a9c5bb5c05df2d18ca30db01972d94c
Roketskyi Roman <91413976+Roketskyi@users.noreply.github.com>
2023-01-12 02:52:30 +0300

    Delete src directory

--- src/pages/posts/Posts.js
+++
@@ -1,9 +0,0 @@
-function Posts() {
-    return (
-      <div>
-        <h2>Enter the text after the "/" sign in the search bar.</h2><br></br>
-      </div>
-    )
-}
-  
-export {Posts};
--- src/pages/video/Video.js
+++
@@ -1,9 +0,0 @@
-function Video() {
-    return (
-      <div>
-        <h2>Video! You can watch the films</h2>
-      </div>
-    )
-}
-  
-export {Video};
