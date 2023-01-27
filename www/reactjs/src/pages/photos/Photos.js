70269b25a016d295d972394da942c2b4d6ad2c27
Roman Roketskyi <romanroketskiy05@gmail.com>
2023-01-27 02:14:58 +0200

    update

---
+++ www/reactjs/src/pages/photos/Photos.js
@@ -0,0 +1,9 @@
+function Photos() {
+    return (
+      <div>
+        <h2>Photos! You can see the mountains</h2>
+      </div>
+    )
+}
+  
+export {Photos};
---
+++ www/reactjs/src/pages/posts/Posts.js
@@ -0,0 +1,11 @@
+import './posts.css';
+
+function Posts() {
+    return (
+      <div>
+        <h2>Enter the text after the "/" sign in the search bar.</h2><br></br>
+      </div>
+    )
+}
+  
+export {Posts};
